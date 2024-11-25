af3f49c3b9bea245f81b95c22c61bc9d23c05c93
rslabiski <rogerio_slabiski@hotmail.com>
2024-11-25 16:34:07 -0300

    trabalho 03: implementacao de heart beat entre quorum e o lider

--- trabalho-03-pyro-e-algoritmo-de-consenso/leader.py
+++ trabalho-03-pyro-e-algoritmo-de-consenso/leader.py
@@ -4,6 +4,15 @@ import time
 import sys
 import threading
 
+stop_event = threading.Event()
+
+def check_heart_beat(server):
+	print('Check Heart beat thread running...')
+	while not stop_event.is_set():
+		server.check_quorum_heart_beat()
+		time.sleep(float(server.heart_beat_timeout))
+	print('Check Heart beat thread finished.')
+
 class BrokerLeader( object ):
 
 	name = 'Lider_Epoca1'
@@ -13,18 +22,22 @@ class BrokerLeader( object ):
 
 	def __init__(self):
 		try:
+			self.heart_beat_timeout = float(5)
 			self.daemon = Daemon()
 			self.uri = self.daemon.register(self)
+			print(f'Leader URI: {self.uri}')
 			print('Searching name server...')
 			self.name_server = locate_ns()
 			self.name_server.register(self.name, self.uri)
-			print(f'Leader URI: {self.uri}')
 		except Exception as e:
 			print(f'Exception: {e}')
 			sys.exit(1)
 
 	def run(self):
 		try:
+			print('Starting heartbeat thread...')
+			self.pulse_thread = threading.Thread(target=check_heart_beat, args=(self,))
+			self.pulse_thread.start()
 			print('Press Ctrl+C to shut down.')
 			self.daemon.requestLoop()
 		finally:
@@ -32,6 +45,7 @@ class BrokerLeader( object ):
 
 	def cleanup(self):
 		try:
+			stop_event.set()  # Sinaliza para a thread encerrar
 			print('Removing from name server...')
 			self.name_server.remove(self.name)
 		except Exception as e:
@@ -52,12 +66,29 @@ class BrokerLeader( object ):
 	@expose
 	@oneway
 	def beat(self, URI):
+		# atualiza o seu tempo no dicionario
 		self.quorum[URI] = time.time()
-		for index, (key, value) in enumerate(self.quorum.items()):
-			if URI == key:
-				print(f'V{index} -: {value:.4f}')
-			else:
-				print(f'V{index}  : {value:.4f}')
+
+	def check_quorum_heart_beat(self):
+		print('Checking heart beat timeout...')
+		members_to_remove = []
+		for index, (member, value) in enumerate(self.quorum.items()):
+			dt = time.time() - value
+			if dt > self.heart_beat_timeout:
+				members_to_remove.append(member)
+				print(f'V{index} timeout!')
+		# precisa remover no final para nao ocorrer problema no loop do dicionario
+		for member in members_to_remove:
+			print(f'URI {member} removed.')
+			self.quorum.pop(member)
+		
+		# verifica promover observers
+		if len(self.quorum) < 2 and len(self.observers):
+			new_member = self.observers.pop()
+			self.quorum[new_member] = time.time()
+			Proxy(new_member).set_state('v')
+			Proxy(new_member).notify()
+			print(f'Observer {str(new_member)[:15]} promoted.')
 
 	@expose
 	@oneway
